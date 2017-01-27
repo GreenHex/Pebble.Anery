74d6323681b2ed3761d3cbd3279cb1a1fee1ebd1
GreenHex <GreenHex@gmail.com>
2017-01-27 15:43:21 +0530

    Larger icons, fine-tuning

--- src/c/weather.c
+++ src/c/weather.c
@@ -93,7 +93,7 @@ void weather_text_layer_update_proc( Layer *layer, GContext *ctx ) {
   // graphics_fill_rect( ctx, weather_text_layer_bounds, 2, GCornersAll );
   graphics_context_set_antialiased( ctx, true );
   weather_text_layer_bounds.origin.y -= WEATHER_TEXT_VERT_ADJ;
-  graphics_context_set_text_color( ctx, GColorWhite );
+  graphics_context_set_text_color( ctx, GColorLightGray );
   graphics_draw_text( ctx, weather_data.temp_str, fonts_get_system_font( FONT_KEY_GOTHIC_28 /* FONT_KEY_ROBOTO_CONDENSED_21 */ ), weather_text_layer_bounds,
                      GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL );
 }
